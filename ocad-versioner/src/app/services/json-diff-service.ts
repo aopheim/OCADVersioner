c988b14717d25e28eb9a47f65238a9d05967c121
aopheim <adrian-o@online.no>
2023-11-28 18:42:25 +0100

    Move test data to separate file

--- ocad-versioner/src/app/services/json-diff-service.ts
+++ ocad-versioner/src/app/services/json-diff-service.ts
@@ -2,17 +2,18 @@ import { Injectable } from '@angular/core';
 import { IJsonDiffService } from './json-diff-service.models';
 import { getDiff } from 'json-difference';
 import { OcadDiffDto } from '../components/ocad-diff-table/ocad-diff-table.models';
+import { diff } from 'json-diff-ts';
 
 @Injectable()
 export class JsonDiffService implements IJsonDiffService {
   getJsonDiff(oldJsonData: any, newJsonData: any): OcadDiffDto {
     console.log('newData', newJsonData);
-    const diffs = getDiff(
+    const diffs = diff(
       JSON.stringify(oldJsonData),
       JSON.stringify(newJsonData),
       {}
     );
-    console.log('diffs: ', diffs.added);
+    console.log('diffs: ', diffs);
 
     return {
       added: [],
