c1447f182f2bba287547259d7ef3b0a092c96173
aopheim <adrian-o@online.no>
2023-12-08 13:49:01 +0100

    Use private ocad2geojson package until new release

--- ocad-versioner/src/app/services/ocad-reader-service.ts
+++ ocad-versioner/src/app/services/ocad-reader-service.ts
@@ -1,4 +1,4 @@
-import * as ocad2geojson from 'ocad2geojson';
+import { readOcad, ocadToGeoJson } from 'ocad2geojson-test';
 import { IOcadReaderService } from './ocad-reader-service.models';
 import { FeatureCollection } from 'geojson';
 import { Injectable } from '@angular/core';
@@ -7,9 +7,9 @@ import { Buffer } from 'buffer';
 @Injectable()
 export class OcadReaderService implements IOcadReaderService {
   async getGeoJsonFromOcdFile(ocadFile: File): Promise<FeatureCollection> {
-    const parsedOcdFile = await ocad2geojson.readOcad(
+    const parsedOcdFile = await readOcad(
       Buffer.from(await ocadFile.arrayBuffer())
     );
-    return ocad2geojson.ocadToGeoJson(parsedOcdFile) as FeatureCollection;
+    return ocadToGeoJson(parsedOcdFile);
   }
 }
